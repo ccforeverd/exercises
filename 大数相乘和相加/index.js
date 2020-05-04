e99056f15104d7fb9dc6a69694d4c8d24de8f5c6
ccforeverd <zh1045456074@163.com>
2020-05-04 15:24:52 +0800

    big number add and multiply

---
+++ 大数相乘和相加/index.js
@@ -0,0 +1,30 @@
+const isBigNumber = n => {
+  return typeof n === 'number' &&  n.toString.indexOf('e') > -1
+}
+
+const numberToString = n => {
+  console.log(n)
+}
+
+const doubleStringAdd = (ns1, ns2) => {
+
+}
+
+
+const bigNumberAdd = (...numbers) => {
+  return numbers.map(numberToString)
+    .reduce(doubleStringAdd, '0')
+}
+
+const bigNumberMultiply = (...numbers) => {
+  return numbers
+}
+
+const test1 = 1.333e31
+const test2 = 5.777e30
+
+console.log(test1 + test2)
+console.log(test1 * test2)
+
+console.log(bigNumberAdd(test1, test2))
+console.log(bigNumberMultiply(test1, test2))
