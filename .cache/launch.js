8d11a4eb0ababdde79df9dff363390e1cb4a9961
ccforeverd <zh1045456074@163.com>
2020-05-07 18:00:52 +0800

    finish

--- .cache/launch.js
+++ .cache/launch.js
@@ -1,76 +1,41 @@
 'use strict';
 
-/**
- * 判断是否是大数
- * @param {Number} n 数值
- * @returns {Boolean}
- */
-const isBigNumber = n => {
-  console.log(1, n);
-  console.log(2, Number(n));
-  console.log(3, Number(n).toString());
-  return Number(n).toString().indexOf('e+') > -1
-};
-
-const bigNumber2Array = n => {
-  
-};
-
-var utils = /*#__PURE__*/Object.freeze({
-  __proto__: null,
-  isBigNumber: isBigNumber,
-  bigNumber2Array: bigNumber2Array
-});
-
-// 位数数组
-
-class BigNumberArray extends Array {
-  constructor (number) {
-    super(0);
-
-    if (typeof number !== 'number') {
-      throw new Error('request a number')
-    }
-    if (!isBigNumber(number)) {
-      throw new Error('request a big number')
-    }
-
-    number = Number(number); // Number(1333e33) => 1.333e+36 相当于格式化
-    number = number.toString(); // 字符串化 => "1.333e+36"
-    number = number.split('e+'); // 分开数值和位数 => ["1.333", "36"]
-
-    const figure = Number(number[1]); // 取到位数, e+右侧 => 36
-
-    number = number[0]; // e+左侧 => "1.333"
-    number = number.replace('.', ''); // 去掉小数点 => "1333"
-    number = [...number]; // 数组化 => ["1", "3", "3", "3"]
-    number = number.map((item, index) => [Number(item), figure - index]); // 位数化 => [[1, 36], [3, 35], [3, 34], [3, 33]]
-
-    // this.concat(number)
-    this.push(...number);
-
-    console.log(JSON.stringify(this));
-  }
-}
-
-Object.assign(BigNumberArray, utils);
-
 const number2Array = n => {
-  // console.log(n)
+  const [number, figure] = Number(n).toString().split('e+');
+  if (figure) {
+    // 大数
+    return [
+      ...[...Array(parseInt(figure) - number.length + 2)].fill(0), // 小数点 + 小数点左侧固定的一位数 = 2
+      ...[...number.replace('.', '')].reverse().map(item => parseInt(item))
+    ]
+  } else {
+    return number.reverse().map(item => parseInt(item))
+  }
 };
 
-const doubleStringAdd = (ns1, ns2) => {
-
+const array2Number = a => {
+  return Number(a.reverse().join(''))
 };
-
 
 const bigNumberAdd = (...numbers) => {
-  return numbers.map(number2Array)
-    .reduce(doubleStringAdd, '0')
+  const arrays = numbers.map(number2Array);
+  const maxLength = Math.max(...arrays.map(item => item.length));
+  const result = [];
+  for (let index = 0; index < maxLength; index++) {
+    const number = (result[index] || 0) + arrays.map(item => item[index] || 0).reduce((r, n) => (r + n), 0);
+    result[index] = number % 10;
+    result[index + 1] = Math.floor(number / 10);
+  }
+  return array2Number(result)
 };
 
 const bigNumberMultiply = (...numbers) => {
-  return numbers
+  return Number(numbers
+    .map(n => Number(n).toString().split('e+'))
+    .reduce((result, [number, figure]) => {
+      return [result[0] * parseFloat(number), result[1] + parseInt(figure || 0)]
+    }, [1, 0])
+    .join('e+'))
 };
 
 const test1 = 1.333e31;
@@ -81,5 +46,3 @@ console.log(test1 * test2);
 
 console.log(bigNumberAdd(test1, test2));
 console.log(bigNumberMultiply(test1, test2));
-
-console.log(new BigNumberArray(1333e33));
--- 大数相乘和相加/index.js
+++ 大数相乘和相加/index.js
@@ -1,23 +1,42 @@
-import BigNumberArray from './BigNumberArray'
 
 const number2Array = n => {
-  // console.log(n)
+  const [number, figure] = Number(n).toString().split('e+')
+  if (figure) {
+    // 大数
+    return [
+      ...[...Array(parseInt(figure) - number.length + 2)].fill(0), // 小数点 + 小数点左侧固定的一位数 = 2
+      ...[...number.replace('.', '')].reverse().map(item => parseInt(item))
+    ]
+  } else {
+    return number.reverse().map(item => parseInt(item))
+  }
 }
 
-const array2Number = a => {}
-
-const doubleStringAdd = (ns1, ns2) => {
-
+const array2Number = a => {
+  return Number(a.reverse().join(''))
 }
-
 
 const bigNumberAdd = (...numbers) => {
-  return numbers.map(number2Array)
-    .reduce(doubleStringAdd, '0')
+  const arrays = numbers.map(number2Array)
+  const maxLength = Math.max(...arrays.map(item => item.length))
+  const result = []
+  for (let index = 0; index < maxLength; index++) {
+    const number = (result[index] || 0) + arrays.map(item => item[index] || 0).reduce((r, n) => (r + n), 0)
+    result[index] = number % 10
+    result[index + 1] = Math.floor(number / 10)
+  }
+  return array2Number(result)
 }
 
 const bigNumberMultiply = (...numbers) => {
-  return numbers
+  return Number(
+    numbers
+      .map(n => Number(n).toString().split('e+'))
+      .reduce((result, [number, figure]) => {
+        return [result[0] * parseFloat(number), result[1] + parseInt(figure || 0)]
+      }, [1, 0])
+      .join('e+')
+  )
 }
 
 const test1 = 1.333e31
@@ -28,5 +47,3 @@ console.log(test1 * test2)
 
 console.log(bigNumberAdd(test1, test2))
 console.log(bigNumberMultiply(test1, test2))
-
-console.log(new BigNumberArray(1333e33))
